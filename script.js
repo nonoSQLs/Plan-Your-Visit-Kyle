4b87bda04addcec83e196d1e8145c22ca7f90151
Kyle Parry <kyle.parry.303@gmail.com>
2021-02-09 19:58:00 -0700

    Added pgServer and new relic script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+/* eslint-disable func-names */
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 1000 },
+    { duration: '10s', target: 1500 },
+    { duration: '10s', target: 2000 },
+    { duration: '10s', target: 2000 },
+    { duration: '10s', target: 1500 },
+    { duration: '10s', target: 1000 },
+  ],
+  // scenarios: {
+  //   constant_request_rate: {
+  //     executor: 'constant-arrival-rate',
+  //     rate: 1000,
+  //     timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+  //     duration: '30s',
+  //     preAllocatedVUs: 100, // how large the initial pool of VUs would be
+  //     maxVUs: 200, // if the preAllocatedVUs are not enough, we can initialize more
+  //   },
+  // },
+};
+
+export default function () {
+  http.get(`http://localhost:8080/api/recommended/${Math.floor(Math.random() * 10000001)}`);
+  sleep(1);
+}
